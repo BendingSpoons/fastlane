939d17482c9c333734dee46016a740dcb4f3dd9c
Alain Caltieri <alaincaltieri@gmail.com>
2018-04-16 11:52:06 +0200

    Add developers response functionality

--- spaceship/lib/spaceship/tunes/app_ratings.rb
+++ spaceship/lib/spaceship/tunes/app_ratings.rb
@@ -27,22 +27,22 @@ module Spaceship
       attr_accessor :five_star_rating_count
 
       attr_mapping({
-        'reviewCount' => :review_count,
-        'ratingCount' => :rating_count,
-        'ratingOneCount' => :one_star_rating_count,
-        'ratingTwoCount' => :two_star_rating_count,
-        'ratingThreeCount' => :three_star_rating_count,
-        'ratingFourCount' => :four_star_rating_count,
-        'ratingFiveCount' => :five_star_rating_count
-      })
+                       'reviewCount' => :review_count,
+                       'ratingCount' => :rating_count,
+                       'ratingOneCount' => :one_star_rating_count,
+                       'ratingTwoCount' => :two_star_rating_count,
+                       'ratingThreeCount' => :three_star_rating_count,
+                       'ratingFourCount' => :four_star_rating_count,
+                       'ratingFiveCount' => :five_star_rating_count
+                   })
 
       # @return (Float) the average rating for this summary (rounded to 2 decimal places)
       def average_rating
         ((one_star_rating_count +
-          (two_star_rating_count * 2) +
-          (three_star_rating_count * 3) +
-          (four_star_rating_count * 4) +
-          (five_star_rating_count * 5)) / rating_count.to_f).round(2)
+            (two_star_rating_count * 2) +
+            (three_star_rating_count * 3) +
+            (four_star_rating_count * 4) +
+            (five_star_rating_count * 5)) / rating_count.to_f).round(2)
       end
 
       # @return (Array) of Review Objects
@@ -52,6 +52,114 @@ module Spaceship
           review["value"]["application"] = self.application
           AppReview.factory(review["value"])
         end
+      end
+    end
+
+    class DeveloperResponse < TunesBase
+      attr_reader :id
+      attr_reader :response
+      attr_reader :last_modified
+      attr_reader :hidden
+      attr_reader :state
+      attr_accessor :application
+      attr_accessor :review_id
+
+      attr_mapping({
+                       'responseId' => :id,
+                       'response' => :response,
+                       'lastModified' => :last_modified,
+                       'isHidden' => :hidden,
+                       'pendingState' => :state
+                   })
+
+      def create!(text)
+        client.create_developer_response!(app_id: application.apple_id, platform: application.platform, review_id: review_id, response: text)
+      end
+
+      def update!(text)
+        client.update_developer_response!(app_id: application.apple_id, platform: application.platform, review_id: review_id, response_id: id, response: text)
+      end
+
+      def create_developer_response!(app_id: nil, platform: "ios", review_id: nil, response: nil)
+        raise "app_id is required" unless app_id
+        raise "review_id is required" unless review_id
+        raise "response is required" unless response
+
+        data = {
+            responseText: response,
+            reviewId: review_id
+        }
+        request(:post) do |req|
+          req.url("ra/apps/#{app_id}/platforms/#{platform}/reviews/#{review_id}/responses")
+          req.body = data.to_json
+          req.headers['Content-Type'] = 'application/json'
+        end
+      end
+
+      def update_developer_response!(app_id: nil, platform: "ios", review_id: nil, response_id: nil, response: nil)
+        raise "app_id is required" unless app_id
+        raise "review_id is required" unless review_id
+        raise "response_id is required" unless response_id
+        raise "response is required" unless response
+
+        data = {
+            responseText: response
+        }
+        request(:put) do |req|
+          req.url("ra/apps/#{app_id}/platforms/#{platform}/reviews/#{review_id}/responses/#{response_id}")
+          req.body = data.to_json
+          req.headers['Content-Type'] = 'application/json'
+        end
+      end
+    end
+
+    class AppReview < TunesBase
+      attr_accessor :application
+      attr_reader :rating
+      attr_reader :id
+      attr_reader :title
+      attr_reader :review
+      attr_reader :nickname
+      attr_reader :store_front
+      attr_reader :app_version
+      attr_reader :last_modified
+      attr_reader :helpful_views
+      attr_reader :total_views
+      attr_reader :edited
+      attr_reader :raw_developer_response
+      attr_accessor :developer_response
+
+      attr_mapping({
+                       'id' => :id,
+                       'rating' => :rating,
+                       'title' => :title,
+                       'review' => :review,
+                       'nickname' => :nickname,
+                       'storeFront' => :store_front,
+                       'appVersionString' => :app_version,
+                       'lastModified' => :last_modified,
+                       'helpfulViews' => :helpful_views,
+                       'totalViews' => :total_views,
+                       'edited' => :edited,
+                       'developerResponse' => :raw_developer_response
+                   })
+      class << self
+        # Create a new object based on a hash.
+        # This is used to create a new object based on the server response.
+        def factory(attrs)
+          obj = self.new(attrs)
+          response_attrs = {}
+          response_attrs = obj.raw_developer_response if obj.raw_developer_response
+          response_attrs[:application] = obj.application
+          response_attrs[:review_id] = obj.id
+          obj.developer_response = DeveloperResponse.factory(response_attrs)
+          return obj
+        end
+      end
+
+      def responded?
+        return true if raw_developer_response
+        false
       end
     end
   end
