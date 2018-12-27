de092ac36ee07c8f95181091d32e62ed5299484e
Simon Vocella <voxsim@gmail.com>
2018-12-27 09:20:07 +0100

    Raise exception when there is an error for loading session from file

--- spaceship/lib/spaceship/client.rb
+++ spaceship/lib/spaceship/client.rb
@@ -568,6 +568,9 @@ module Spaceship
         return true
       end
       return false
+    rescue => ex
+      puts(ex.to_s)
+      raise BasicPreferredInfoError.new, "Error loading session from #{persistent_cookie_path}"
     end
 
     def load_session_from_env
