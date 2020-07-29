af72b22c01d49bb872ece240216debea28e16b70
valerio-castelli <vc@bendingspoons.com>
2020-07-29 16:56:35 +0200

    feat: add support for eula management

--- spaceship/lib/spaceship/connect_api/models/territory.rb
+++ spaceship/lib/spaceship/connect_api/models/territory.rb
@@ -23,6 +23,11 @@ module Spaceship
         resps = client.get_territories(filter: {}, includes: nil, limit: nil, sort: nil).all_pages
         return resps.flat_map(&:to_models)
       end
+
+      def self.for_eula(end_user_license_agreement_id: nil)
+        resps = Spaceship::ConnectAPI.get_eula_territories(end_user_license_agreement_id: end_user_license_agreement_id).all_pages
+        return resps.flat_map(&:to_models)
+      end
     end
   end
 end
