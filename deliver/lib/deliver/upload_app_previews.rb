48b0fa06586ef11e7b69607a589474af5b381824
Ivan Vrsajkov <vrsajkov.ivan@gmail.com>
2020-08-31 11:16:41 +0200

    fix: create missing app store preview sets

--- deliver/lib/deliver/upload_app_previews.rb
+++ deliver/lib/deliver/upload_app_previews.rb
@@ -29,7 +29,7 @@ module Deliver
 
       app_store_preview_sets_map = load_app_store_preview_sets(localizations)
 
-      changed_sets_per_language = get_changed_previews(app_store_preview_sets_map, candidate_previews_per_language)
+      changed_sets_per_language = get_changed_previews(localizations, app_store_preview_sets_map, candidate_previews_per_language)
       app_store_previews_to_delete = get_app_store_previews_to_delete(app_store_preview_sets_map, changed_sets_per_language)
 
       delete_app_previews(localizations, app_store_preview_sets_map, app_store_previews_to_delete, max_n_threads)
@@ -74,7 +74,7 @@ module Deliver
       app_store_preview_sets_map
     end
 
-    def get_changed_previews(app_store_preview_sets_map, candidate_previews_per_language)
+    def get_changed_previews(localizations, app_store_preview_sets_map, candidate_previews_per_language)
       candidate_sets_per_language = {} # per locale and type; all provided previews
       changed_sets_per_language = {} # per locale and type; previews that have been added, removed or updated
 
@@ -112,6 +112,16 @@ module Deliver
 
       # then, compare the new previews with the existing ones
       candidate_sets_per_language.each do |language, candidate_sets_per_preview_type|
+        # Find localization to upload app previews to
+        localization = localizations.find do |l|
+          l.locale == language
+        end
+
+        unless localization
+          UI.error("Couldn't find localization on version for #{language}")
+          next
+        end
+
         changed_previews_per_type = {}
 
         unless app_store_preview_sets_map.key?(language)
@@ -121,7 +131,19 @@ module Deliver
 
         app_store_sets_per_preview_type = app_store_preview_sets_map[language]
         candidate_sets_per_preview_type.each do |preview_type, candidates_with_checksums|
+          app_store_preview_set = app_store_sets_per_preview_type[preview_type]
+
+          unless app_store_preview_set
+            app_store_preview_set = localization.create_app_preview_set(attributes: {
+                previewType: preview_type
+            })
+            app_store_sets_per_preview_type[preview_type] = app_store_preview_set
+          end
+
           app_store_previews = app_store_sets_per_preview_type[preview_type].app_previews
+          # in case the set has just been created on App Store, it's app_previews can be nil
+          app_store_previews ||= []
+
           changed_previews_per_type[preview_type] ||= []
 
           candidates_with_checksums.each_with_index do |candidate_with_checksum, index|
@@ -313,16 +335,6 @@ module Deliver
 
       n_threads = [max_n_threads, changed_sets_per_language.keys.length].min
       Parallel.each(changed_sets_per_language, in_threads: n_threads) do |language, changed_sets_per_preview_type|
-        # Find localization to upload app previews to
-        localization = localizations.find do |l|
-          l.locale == language
-        end
-
-        unless localization
-          UI.error("Couldn't find localization on version for #{language}")
-          next
-        end
-
         app_store_sets_for_language = app_store_preview_sets_map[language]
         uploaded_app_store_previews_per_preview_type = {}
 
@@ -340,13 +352,6 @@ module Deliver
 
             position = changed_preview_with_position[:position]
             app_store_preview_set = app_store_sets_for_language[preview_type]
-
-            unless app_store_preview_set
-              app_store_preview_set = localization.create_app_preview_set(attributes: {
-                  previewType: preview_type
-              })
-              app_store_sets_for_language[preview_type] = app_store_preview_set
-            end
 
             Deliver.retry_api_call do
               UI.message("Uploading '#{changed_preview.path}'...")
